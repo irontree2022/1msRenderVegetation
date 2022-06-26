c75046c12171c5cc920432885ede32b05bcfc829
irontree2022 <irontree2022@163.com>
2022-06-26 20:49:56 +0800

    修改代码，添加同步和异步从ComouteBuffer中获取数据的实现

--- Assets/Demo/ComputeShaderDemo/ComputeShaderDemo.cs
+++ Assets/Demo/ComputeShaderDemo/ComputeShaderDemo.cs
@@ -13,29 +13,72 @@ public class ComputeShaderDemo : MonoBehaviour
 {
     public Camera MainCamera;
     public GameObject Prefab;
-    public ComputeShader FrustumCullingComputeShader;
     public int InstanceCount = 100000;
     public Vector3Int InstanceExtents = new Vector3Int(500, 500, 500);
-    [Range(0.1f, 10)]
     public float RandomeMaxScaleValue = 5;
-    public Bounds DrawBounds = new Bounds();
+    public ComputeShader FrustumCullingComputeShader;
+    public uint instanceBoundsCount = 0;
+    [Header("调试：显示实例包围盒 GetData()")]
+    public bool showInstanceBounds_GetData;
+    [Header("调试：显示实例包围盒 AsyncRequest")]
+    public bool showInstanceBounds_AsyncRequest;
 
-    private Bounds meshBounds;
-    private Mesh mesh;
-    private Material material;
+
     private int FrustumCullingKernel;
     private ComputeBuffer instanceInputBuffer;
     private ComputeBuffer instanceOutputBuffer;
     private ComputeBuffer instanceBoundsBuffer;
-    private GPUBounds[] instanceBounds;
-    private uint[] instanceBoundsCount = new uint[] { 0 };
     private ComputeBuffer instanceBoundsCountBuffer;
+    private Vector4[] FrustumPlanes = new Vector4[6];
+    private uint[] instanceBoundsCountArray = new uint[] { 0 };
+    private GPUBounds[] instanceBounds;
+    private bool waitRR;
+    private bool instanceBoundsCount_ok = true;
+    private bool instanceBoundsData_ok = true;
+    private UnityEngine.Rendering.AsyncGPUReadbackRequest instanceBoundsDataRR;
+    private UnityEngine.Rendering.AsyncGPUReadbackRequest instanceBoundsCountRR;
+
     private ComputeBuffer argsBuffer;
     private uint[] args = new uint[5] { 0, 0, 0, 0, 0 };
+    private Bounds meshBounds;
+    private Mesh mesh;
+    private Material material;
+    private Bounds DrawBounds = new Bounds();
     private MaterialPropertyBlock mpb;
-    private Vector4[] FrustumPlanes = new Vector4[6];
+
+
+    void Start()
+    {
+        FrustumCullingKernel = FrustumCullingComputeShader.FindKernel("FrustumCulling");
+        mesh = Prefab.GetComponent<MeshFilter>().sharedMesh;
+        var mr = Prefab.GetComponent<MeshRenderer>();
+        material = mr.sharedMaterial;
+        meshBounds = mr.bounds;
+        args[0] = mesh.GetIndexCount(0);
+        args[1] = 0;
+        args[2] = mesh.GetIndexStart(0);
+        args[3] = mesh.GetBaseVertex(0);
+        args[4] = 0;
+        argsBuffer = new ComputeBuffer(5, sizeof(uint) * 5, ComputeBufferType.IndirectArguments);
+        argsBuffer.SetData(args);
+        instanceInputBuffer = new ComputeBuffer(InstanceCount, sizeof(float) * 16);
+        instanceInputBuffer.SetData(RandomGeneratedInstances(InstanceCount, InstanceExtents, RandomeMaxScaleValue));
+        instanceOutputBuffer = new ComputeBuffer(InstanceCount, sizeof(float) * 16, ComputeBufferType.Append);
+        instanceBoundsBuffer = new ComputeBuffer(InstanceCount, sizeof(float) * 3 * 2, ComputeBufferType.Append);
+        instanceBoundsCountBuffer = new ComputeBuffer(1, sizeof(uint), ComputeBufferType.IndirectArguments);
+        mpb = new MaterialPropertyBlock();
+        mpb.SetBuffer("IndirectShaderDataBuffer", instanceOutputBuffer);
+        DrawBounds.size = Vector3.one * 10000;
+
+        FrustumCullingComputeShader.SetInt("inputCount", instanceInputBuffer.count);
+        FrustumCullingComputeShader.SetVector("boxCenter", meshBounds.center);
+        FrustumCullingComputeShader.SetVector("boxExtents", meshBounds.extents);
+        FrustumCullingComputeShader.SetBuffer(FrustumCullingKernel, "input", instanceInputBuffer);
+        FrustumCullingComputeShader.SetBuffer(FrustumCullingKernel, "VisibleBuffer", instanceOutputBuffer);
+        FrustumCullingComputeShader.SetBuffer(FrustumCullingKernel, "GPUBoundsBuffer", instanceBoundsBuffer);
+    }
     // 随机生成10万个实例矩阵
-    private Matrix4x4[] RandomlyGenerated(int instanceCount, Vector3Int instanceExtents, float maxScale)
+    private Matrix4x4[] RandomGeneratedInstances(int instanceCount, Vector3Int instanceExtents, float maxScale)
     {
         var instances = new Matrix4x4[instanceCount];
         var cameraPos = MainCamera.transform.position;
@@ -51,45 +94,6 @@ public class ComputeShaderDemo : MonoBehaviour
         }
         return instances;
     }
-    private void initData()
-    {
-        FrustumCullingKernel = FrustumCullingComputeShader.FindKernel("FrustumCulling");
-        mesh = Prefab.GetComponent<MeshFilter>().sharedMesh;
-        var mr = Prefab.GetComponent<MeshRenderer>();
-        material = mr.sharedMaterial;
-        meshBounds = mr.bounds;
-        args[0] = mesh.GetIndexCount(0);
-        args[1] = 0;
-        args[2] = mesh.GetIndexStart(0);
-        args[3] = mesh.GetBaseVertex(0);
-        args[4] = 0;
-        argsBuffer = new ComputeBuffer(5, sizeof(uint) * 5, ComputeBufferType.IndirectArguments);
-        argsBuffer.SetData(args);
-        instanceInputBuffer = new ComputeBuffer(InstanceCount, sizeof(float) * 16);
-        instanceInputBuffer.SetData(RandomlyGenerated(InstanceCount, InstanceExtents, RandomeMaxScaleValue));
-        instanceOutputBuffer = new ComputeBuffer(InstanceCount, sizeof(float) * 16, ComputeBufferType.Append);
-        instanceBoundsBuffer = new ComputeBuffer(InstanceCount, sizeof(float) * 3 * 2, ComputeBufferType.Append);
-        instanceBoundsCountBuffer = new ComputeBuffer(1, sizeof(uint), ComputeBufferType.IndirectArguments);
-        mpb = new MaterialPropertyBlock();
-        mpb.SetBuffer("IndirectShaderDataBuffer", instanceOutputBuffer);
-        DrawBounds.size = Vector3.one * 10000;
-    }
-    private void SetComputeShaderArgs()
-    {
-        FrustumCullingComputeShader.SetInt("inputCount", instanceInputBuffer.count);
-        FrustumCullingComputeShader.SetVector("boxCenter", meshBounds.center);
-        FrustumCullingComputeShader.SetVector("boxExtents", meshBounds.extents);
-        FrustumCullingComputeShader.SetBuffer(FrustumCullingKernel, "input", instanceInputBuffer);
-        FrustumCullingComputeShader.SetBuffer(FrustumCullingKernel, "VisibleBuffer", instanceOutputBuffer);
-        FrustumCullingComputeShader.SetBuffer(FrustumCullingKernel, "GPUBoundsBuffer", instanceBoundsBuffer);
-
-    }
-    void Start()
-    {
-        initData();
-        SetComputeShaderArgs();
-    }
-
 
     private Vector3 per_playerPos = Vector3.zero;
     private Quaternion per_playerRot = Quaternion.identity;
@@ -138,49 +142,89 @@ public class ComputeShaderDemo : MonoBehaviour
         planes[5] = GetPlane(transform.forward, transform.position + transform.forward * camera.farClipPlane);//far
         return planes;
     }
-    private void UpdateBuffer()
-    {
-        instanceOutputBuffer.SetCounterValue(0);
-        instanceBoundsBuffer.SetCounterValue(0);
-        FrustumCullingComputeShader.SetVectorArray("cameraPlanes", GetFrustumPlanes(MainCamera, FrustumPlanes));
-        int threadGroupsX = InstanceCount / 32;
-        if (InstanceCount % 32 != 0) ++threadGroupsX;
-        FrustumCullingComputeShader.Dispatch(FrustumCullingKernel, threadGroupsX, 1, 1);
-        ComputeBuffer.CopyCount(instanceOutputBuffer, argsBuffer, sizeof(uint));
-        DrawBounds.center = MainCamera.transform.position;
-        ComputeBuffer.CopyCount(instanceBoundsBuffer, instanceBoundsCountBuffer, 0);
-        instanceBoundsCountBuffer.GetData(instanceBoundsCount);
-        if (instanceBounds == null || instanceBounds.Length != instanceBoundsCount[0])
-            instanceBounds = new GPUBounds[instanceBoundsCount[0]];
-        instanceBoundsBuffer.GetData(instanceBounds);
-    }
-    private void Draw()
-    {
-        UnityEngine.Profiling.Profiler.BeginSample("DrawMeshInstancedIndirect");
-        if(!material.enableInstancing) material.enableInstancing = true;
-        Graphics.DrawMeshInstancedIndirect(
-                    mesh,
-                    0,
-                    material,
-                    DrawBounds,
-                    argsBuffer,
-                    0,
-                    mpb
-                    );
-        UnityEngine.Profiling.Profiler.EndSample();
-
-    }
     void Update()
     {
+        if (!instanceBoundsCount_ok && instanceBoundsCountRR.done)
+        {
+            instanceBoundsCount_ok = true;
+            if (!instanceBoundsCountRR.hasError)
+            {
+                instanceBoundsCountRR.GetData<uint>(0).CopyTo(instanceBoundsCountArray);
+                instanceBoundsCount = instanceBoundsCountArray[0];
+                if (instanceBoundsCount > 0)
+                {
+                    instanceBoundsData_ok = false;
+                    instanceBoundsDataRR = UnityEngine.Rendering.AsyncGPUReadback.Request(instanceBoundsBuffer, (int)instanceBoundsCount * sizeof(float) * (3 + 3), 0);
+                }
+                else instanceBoundsCount = 0;
+            }
+            else instanceBoundsCount = 0;
+        }
+        if (!instanceBoundsData_ok && instanceBoundsCount_ok && instanceBoundsDataRR.done)
+        {
+            instanceBoundsData_ok = true;
+            if (!instanceBoundsDataRR.hasError)
+            {
+                if (instanceBounds == null || instanceBounds.Length != instanceBoundsCount)
+                    instanceBounds = new GPUBounds[instanceBoundsCount];
+                instanceBoundsDataRR.GetData<GPUBounds>(0).CopyTo(instanceBounds);
+            }
+            else instanceBoundsCount = 0;
+        }
+        if (instanceBoundsCount_ok &&
+            instanceBoundsData_ok)
+            waitRR = false;
+
         if (IsRenderCameraChange())
-            UpdateBuffer();
-        Draw();
+        {
+            DrawBounds.center = MainCamera.transform.position;
+
+            instanceOutputBuffer.SetCounterValue(0);
+            instanceBoundsBuffer.SetCounterValue(0);
+            FrustumCullingComputeShader.SetVectorArray("cameraPlanes", GetFrustumPlanes(MainCamera, FrustumPlanes));
+            FrustumCullingComputeShader.SetBool("showInstanceBounds", showInstanceBounds_GetData || showInstanceBounds_AsyncRequest);
+
+            int threadGroupsX = InstanceCount / 32;
+            if (InstanceCount % 32 != 0) ++threadGroupsX;
+            FrustumCullingComputeShader.Dispatch(FrustumCullingKernel, threadGroupsX, 1, 1);
+            ComputeBuffer.CopyCount(instanceOutputBuffer, argsBuffer, sizeof(uint));
+            if (showInstanceBounds_GetData)
+            {
+                ComputeBuffer.CopyCount(instanceBoundsBuffer, instanceBoundsCountBuffer, 0);
+                instanceBoundsCountBuffer.GetData(instanceBoundsCountArray);
+                instanceBoundsCount = instanceBoundsCountArray[0];
+                if (instanceBounds == null || instanceBounds.Length != instanceBoundsCount)
+                    instanceBounds = new GPUBounds[instanceBoundsCount];
+                instanceBoundsBuffer.GetData(instanceBounds);
+                instanceBoundsCount_ok = true;
+                instanceBoundsData_ok = true;
+            }
+
+            if (!waitRR && showInstanceBounds_AsyncRequest)
+            {
+                waitRR = true;
+                instanceBoundsCount_ok = true;
+                instanceBoundsData_ok = true;
+                ComputeBuffer.CopyCount(instanceBoundsBuffer, instanceBoundsCountBuffer, 0);
+                instanceBoundsCount_ok = false;
+                instanceBoundsCountRR = UnityEngine.Rendering.AsyncGPUReadback.Request(instanceBoundsCountBuffer);
+            }
+        }
+        Graphics.DrawMeshInstancedIndirect(
+            mesh,
+            0,
+            material,
+            DrawBounds,
+            argsBuffer,
+            0,
+            mpb
+            );
     }
 
     private void OnDrawGizmos()
     {
         Gizmos.color = Color.red;
-        for (var i = 0; i < instanceBoundsCount[0]; i++)
+        for (var i = 0; instanceBounds != null && instanceBoundsData_ok && i < instanceBoundsCount; i++)
         {
             var gpubounds = instanceBounds[i];
             Gizmos.DrawCube((gpubounds.min + gpubounds.max) / 2f, gpubounds.max - gpubounds.min);
